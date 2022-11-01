32d1ed2ba6b31cdb1e211081d9b4492b5ad5678a
Chocolate1999 <2816778271@qq.com>
2022-11-01 22:08:37 +0800

    chore: add new project

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center">react-china-division</h1>
+
+<p align="center">
+react-china-division
+</p>
+
+<p align="center">
+<a href="https://space.bilibili.com/351534170"><img src="https://img.shields.io/badge/dynamic/json?labelColor=FE7398&logo=bilibili&logoColor=white&label=bilibili%20fans&color=00aeec&query=%24.data.totalSubs&url=https%3A%2F%2Fapi.spencerwoo.com%2Fsubstats%2F%3Fsource%3Dbilibili%26queryKey%3D351534170" /></a>
+<a href="https://github.com/Chocolate1999/react-china-division" target="__blank"><img alt="GitHub stars" src="https://img.shields.io/github/stars/Chocolate1999/react-china-division?style=social"></a>
+<a href="https://github.com/Chocolate1999/react-china-division/network"><img alt="GitHub forks" src="https://img.shields.io/github/forks/Chocolate1999/react-china-division?style=social"></a>
+<a href="https://github.com/Chocolate1999/react-china-division" target="__blank"><img alt="MIT" src="https://img.shields.io/github/license/Chocolate1999/react-china-division"></a>
+<a href="https://github.com/Chocolate1999/react-china-division" target="__blank"><img alt="React" src="https://img.shields.io/badge/-React-%23282C34?style=flat-square&logo=react"></a>
+</p>
+
+<p align="center">
+ 🧑‍💻👩‍💻👨‍💻
+</p>
+
+## ⚡️ B 站
+
+
+## 🧑‍💻 Contribution
+
+Thank you to all the people who already contributed to my project!
+
+<a href="https://github.com/Chocolate1999/react-china-division/graphs/contributors"><img src="https://github.com/Chocolate1999/react-china-division/blob/main/CONTRIBUTORS.svg" /></a>
+
+## 📄 License
+
+[MIT License](https://github.com/Chocolate1999/react-china-division/blob/main/LICENSE) © 2022-PRESENT [Chocolate](https://github.com/Chocolate1999)
---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Cascader, version } from "antd";
+import "antd/dist/antd.css";
+
+import provinces from "china-division/dist/provinces.json";
+import cities from "china-division/dist/cities.json";
+import areas from "china-division/dist/areas.json";
+
+type ChidDivisionType = {
+  code?: string;
+  name?: string;
+  provinceCode?: string;
+  cityCode?: string;
+  label?: string;
+  value?: string;
+  children?: Array<ChidDivisionType>;
+};
+
+areas.forEach((area: ChidDivisionType) => {
+  const matchCity: ChidDivisionType = cities.filter(
+    (city) => city.code === area.cityCode
+  )[0];
+  if (matchCity) {
+    matchCity.children = matchCity.children || [];
+    matchCity.children.push({
+      label: area.name,
+      value: area.code,
+    });
+  }
+});
+
+cities.forEach((city: ChidDivisionType) => {
+  const matchProvince: ChidDivisionType = provinces.filter(
+    (province) => province.code === city.provinceCode
+  )[0];
+  if (matchProvince) {
+    matchProvince.children = matchProvince.children || [];
+    matchProvince.children.push({
+      label: city.name,
+      value: city.code,
+      children: city.children,
+    });
+  }
+});
+
+const options = provinces.map((province: ChidDivisionType) => ({
+  label: province.name,
+  value: province.code,
+  children: province.children,
+}));
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>react-china-division-demo</p>
+        <p style={{ marginBottom: 24 }}>
+          Current antd version: {version} <br />
+        </p>
+        <Cascader
+          options={options}
+          showSearch
+          placeholder="请选择地址"
+          style={{ width: 400 }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
