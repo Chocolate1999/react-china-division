182f6acdc1e8e5e2dfb6a21c44f90f2992ab3a79
Chocolate1999 <2816778271@qq.com>
2022-11-05 23:49:05 +0800

    feat: 使用新发布的 npm 包

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,7 @@
 import reactLogo from "./assets/react.svg";
+import { AddressCascader } from "react-china-division";
+
 import "./App.css";
-import { Cascader, version } from "antd";
-import "antd/dist/antd.css";
-
-import provinces from "china-division/dist/provinces.json";
-import cities from "china-division/dist/cities.json";
-import areas from "china-division/dist/areas.json";
-
-type ChidDivisionType = {
-  code?: string;
-  name?: string;
-  provinceCode?: string;
-  cityCode?: string;
-  label?: string;
-  value?: string;
-  children?: Array<ChidDivisionType>;
-};
-// TODO: 如果需要区级，可以打开注释
-// areas.forEach((area: ChidDivisionType) => {
-//   const matchCity: ChidDivisionType = cities.filter(
-//     (city) => city.code === area.cityCode
-//   )[0];
-//   if (matchCity) {
-//     matchCity.children = matchCity.children || [];
-//     matchCity.children.push({
-//       label: area.name,
-//       value: area.code,
-//     });
-//   }
-// });
-
-cities.forEach((city: ChidDivisionType) => {
-  const matchProvince: ChidDivisionType = provinces.filter(
-    (province) => province.code === city.provinceCode
-  )[0];
-  if (matchProvince) {
-    matchProvince.children = matchProvince.children || [];
-    matchProvince.children.push({
-      label: city.name,
-      value: city.code,
-      children: city.children,
-    });
-  }
-});
-
-const options = provinces.map((province: ChidDivisionType) => ({
-  label: province.name,
-  value: province.code,
-  children: province.children,
-}));
-
 function App() {
   return (
     <div className="App">
@@ -64,15 +16,16 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <p>react-china-division-demo</p>
-        <p style={{ marginBottom: 24 }}>
-          Current antd version: {version} <br />
-        </p>
-        <Cascader
-          options={options}
-          showSearch
-          placeholder="请选择地址"
-          style={{ width: 400 }}
-        />
+        <p style={{ marginBottom: 24 }}></p>
+        <div>
+          <p>仅展示省市：</p>
+          <AddressCascader />
+        </div>
+        <p style={{ marginBottom: 24 }}></p>
+        <div>
+          <p>全展示：</p>
+          <AddressCascader showArea />
+        </div>
       </div>
     </div>
   );
